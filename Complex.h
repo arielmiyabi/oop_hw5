9186b3ec4a9357771c403a56ac3268213262bf4a
arielmiyabi <arielmiyabi@gmail.com>
2016-06-01 09:26:14 +0800

    新增namespace和更新為Rational型態

--- Complex.h
+++ Complex.h
@@ -1,18 +1,28 @@
 #include <iostream>
+#include "Rational.h"
+
 using namespace std;
 
+namespace ComplexNumber
+{
 class Complex
 {
 public:
     Complex();
-    //ComplexNum(int realNum);
-    Complex(int newImaginaryNum);
-    Complex(int newRealNum, int newImaginaryNum);
-    int getRealNum() const;
-    int getImaginaryNum() const;
+    //ComplexNum(Rational realNum);
+    Complex(Rational newImaginaryNum);
+    Complex(Rational newRealNum, Rational newImaginaryNum);
+    Rational getRealNum() const;
+    Rational getImaginaryNum() const;
+    friend const Complex operator +(const Complex&num1, const Complex&num2);
+    friend const Complex operator -(const Complex&num1, const Complex&num2);
+    friend const Complex operator *(const Complex&num1, const Complex&num2);
+    friend const Complex operator /(const Complex&num1, const Complex&num2);
+    friend const Complex operator -(const Complex&num1);
     friend istream& operator >>(istream& input, Complex& num);
     friend ostream& operator <<(ostream& output, const Complex& num);
 private:
-    int realNum;
-    int imaginaryNum;
+    Rational realNum;
+    Rational imaginaryNum;
 };
+}
