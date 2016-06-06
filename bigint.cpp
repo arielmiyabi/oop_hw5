46555c749090b9885f82510d43d2db45abdbf9a0
yoyo-checkout <stem597sag445@yahoo.com.tw>
2016-06-06 17:31:34 +0800

    Update bigint.cpp

--- bigint.cpp
+++ bigint.cpp
@@ -13,7 +13,14 @@ namespace BigInt_Yo
     ostream& operator <<(ostream& outputstream, const BigInt &a)
     {
         BigInt cpy_a(a);
-        outputstream << cpy_a.getBigint() << endl;
+        if(cpy_a.getSign()==0)
+        {
+            outputstream << "-" << cpy_a.getBigint() << endl;
+        }
+        else
+        {
+            outputstream << cpy_a.getBigint() << endl;
+        }
         return outputstream;
     }
 
@@ -27,72 +34,126 @@ namespace BigInt_Yo
 
     BigInt::BigInt()
     {
+        sign = 1;
         bigInteger = NULL;
         length = 0;
     }
 
-    BigInt::BigInt(char arr[])
+    /*BigInt::BigInt(char arr[])
     {
         int countOfZero = 0, n = 0, m = 0;
+        if(arr[0]=='-')
+        {
+            n++;
+        }
         while(arr[n]=='0')
         {
             countOfZero++;
             n++;
         }
-        char arr2[strlen(arr)-countOfZero+1];
-        for(n = countOfZero;n<strlen(arr);n++)
+        if(arr[0]=='-')
         {
-            arr2[m++] = arr[n];
+            n++;
+            m++;
+            char arr2[strlen(arr)-countOfZero+1];
+            arr2[0] = '-';
+            for(n = countOfZero+1; n<strlen(arr); n++)
+            {
+                arr2[m++] = arr[n];
+            }
+            arr2[m] = '\0';
+            length = strlen(arr2)-1;
+            bigInteger = new char[length+2];
+            for(n = 0; n<length+1; n++)
+            {
+                bigInteger[n] = arr2[n];
+            }
+            bigInteger[n] = '\0';
         }
-        arr2[m] = '\0';
-        length = strlen(arr2);
-        bigInteger = new char[length+1];
-        for(n = 0;n<length;n++){
-            bigInteger[n] = arr2[n];
+        else
+        {
+            char arr2[strlen(arr)-countOfZero+1];
+            for(n = countOfZero; n<strlen(arr); n++)
+            {
+                arr2[m++] = arr[n];
+            }
+            arr2[m] = '\0';
+            length = strlen(arr2);
+            bigInteger = new char[length+1];
+            for(n = 0; n<length; n++)
+            {
+                bigInteger[n] = arr2[n];
+            }
+            bigInteger[n] = '\0';
         }
-        bigInteger[n] = '\0';
-    }
+    }*/
 
     BigInt::BigInt(int integer)
     {
-        char tmp[11];
-        sprintf(tmp, "%d", integer);
-        bigInteger = new char[strlen(tmp)+1];
-        int n;
-        for(n = 0;n<strlen(tmp);n++)
+        if(integer<0)
         {
-            bigInteger[n] = tmp[n];
+            sign = 0;
+            char tmp[12];
+            sprintf(tmp, "%d", integer);
+            bigInteger = new char[strlen(tmp)];
+            int n;
+            for(n = 0; n<strlen(tmp)-1; n++)
+            {
+                bigInteger[n] = tmp[n+1];
+            }
+            bigInteger[n] = '\0';
+            length = strlen(tmp)-1;
         }
-        bigInteger[n] = '\0';
-        length = strlen(tmp);
+        else
+        {
+            sign = 1;
+            char tmp[11];
+            sprintf(tmp, "%d", integer);
+            bigInteger = new char[strlen(tmp)+1];
+            int n;
+            for(n = 0; n<strlen(tmp); n++)
+            {
+                bigInteger[n] = tmp[n];
+            }
+            bigInteger[n] = '\0';
+            length = strlen(tmp);
+        }
     }
 
     BigInt::BigInt(const BigInt &iniObj)
     {
+        sign = iniObj.sign;
+        length = iniObj.length;
         int n;
-        bigInteger = new char[strlen(iniObj.bigInteger)+1];
-        for(n = 0;n<strlen(iniObj.bigInteger);n++)
+        bigInteger = new char[iniObj.length+1];
+        for(n = 0;n<iniObj.length;n++)
         {
             bigInteger[n] = iniObj.bigInteger[n];
         }
         bigInteger[n] = '\0';
-        length = strlen(iniObj.bigInteger);
     }
 
     BigInt::BigInt(string stringOfNumber)
     {
-        if(stringOfNumber.c_str()[strlen(stringOfNumber.c_str())-1]=='!')
+        if(stringOfNumber.c_str()[0]=='-')
         {
-            BigInt fac = factorial(stringOfNumber);
-            length = fac.getLength();
-            bigInteger = fac.getBigint();
+            sign = 0;
+            length = strlen(stringOfNumber.c_str())-1;
+            bigInteger = new char[length+1];
+            int n = 0;
+            for(;n<length;n++)
+            {
+                bigInteger[n] = stringOfNumber.c_str()[n+1];
+            }
+            bigInteger[n] = '\0';
         }
         else
         {
+            sign = 1;
             length = strlen(stringOfNumber.c_str());
             bigInteger = new char[length+1];
             int n = 0;
-            for(; n<length; n++)
+            for(;n<length;n++)
             {
                 bigInteger[n] = stringOfNumber.c_str()[n];
             }
@@ -116,6 +177,11 @@ namespace BigInt_Yo
         return bigInteger;
     }
 
+    int BigInt::getSign()
+    {
+        return sign;
+    }
+
     /*int& Rational::operator [](int a)
     {
         if(a==0) return numerator;
@@ -129,6 +195,19 @@ namespace BigInt_Yo
     bool operator !=(const BigInt& a, const BigInt& b)
     {
         BigInt cpy_a(a), cpy_b(b);
+        if(cpy_a.getSign()==0||cpy_b.getSign()==1)
+        {
+            if(cpy_a.getSign()==cpy_b.getSign())
+            {
+                if(cpy_a.getLength()==cpy_b.getLength())
+                {
+                    if(strcmp(cpy_a.getBigint(), cpy_b.getBigint())!=0) return true;
+                    else return false;
+                }
+                else return true;
+            }
+            else return true;
+        }
         if(cpy_a.getLength()==cpy_b.getLength())
         {
             if(strcmp(cpy_a.getBigint(), cpy_b.getBigint())!=0) return true;
@@ -140,6 +219,19 @@ namespace BigInt_Yo
     bool operator ==(const BigInt& a, const BigInt& b)
     {
         BigInt cpy_a(a), cpy_b(b);
+        if(cpy_a.getSign()==0||cpy_b.getSign()==0)
+        {
+            if(cpy_a.getSign()==cpy_b.getSign())
+            {
+                if(cpy_a.getLength()==cpy_b.getLength())
+                {
+                    if(strcmp(cpy_a.getBigint(), cpy_b.getBigint())==0) return true;
+                    else return false;
+                }
+                else return false;
+            }
+            else return false;
+        }
         if((cpy_a.getLength()==cpy_b.getLength())&&(strcmp(cpy_a.getBigint(), cpy_b.getBigint())==0)) return true;
         else return false;
     }
@@ -147,6 +239,21 @@ namespace BigInt_Yo
     bool operator >=(const BigInt& a, const BigInt& b)
     {
         BigInt cpy_a(a), cpy_b(b);
+        if(cpy_a.getSign()==0||cpy_b.getSign()==0)
+        {
+            if(cpy_a.getSign()==1&&cpy_b.getSign()==0) return true;
+            else if(cpy_a.getSign()==0&&cpy_b.getSign()==1) return false;
+            else
+            {
+                if(cpy_a.getLength()<cpy_b.getLength()) return true;
+                else if(cpy_a.getLength()==cpy_b.getLength())
+                {
+                    if(strcmp(cpy_a.getBigint(), cpy_b.getBigint())<=0) return true;
+                    else return false;
+                }
+                else return false;
+            }
+        }
         if(cpy_a.getLength()>cpy_b.getLength()) return true;
         else if((cpy_a.getLength()==cpy_b.getLength())&&(strcmp(cpy_a.getBigint(), cpy_b.getBigint())>=0)) return true;
         else return false;
@@ -155,6 +262,21 @@ namespace BigInt_Yo
     bool operator >(const BigInt& a, const BigInt& b)
     {
         BigInt cpy_a(a), cpy_b(b);
+        if(cpy_a.getSign()==0||cpy_b.getSign()==0)
+        {
+            if(cpy_a.getSign()==1&&cpy_b.getSign()==0) return true;
+            else if(cpy_a.getSign()==0&&cpy_b.getSign()==1) return false;
+            else
+            {
+                if(cpy_a.getLength()<cpy_b.getLength()) return true;
+                else if(cpy_a.getLength()==cpy_b.getLength())
+                {
+                    if(strcmp(cpy_a.getBigint(), cpy_b.getBigint())<0) return true;
+                    else return false;
+                }
+                else return false;
+            }
+        }
         if(cpy_a.getLength()>cpy_b.getLength()) return true;
         else if((cpy_a.getLength()==cpy_b.getLength())&&(strcmp(cpy_a.getBigint(), cpy_b.getBigint())>0)) return true;
         else return false;
@@ -163,6 +285,21 @@ namespace BigInt_Yo
     bool operator <=(const BigInt& a, const BigInt& b)
     {
         BigInt cpy_a(a), cpy_b(b);
+        if(cpy_a.getSign()==0||cpy_b.getSign()==0)
+        {
+            if(cpy_a.getSign()==1&&cpy_b.getSign()==0) return false;
+            else if(cpy_a.getSign()==0&&cpy_b.getSign()==1) return true;
+            else
+            {
+                if(cpy_a.getLength()<cpy_b.getLength()) return false;
+                else if(cpy_a.getLength()==cpy_b.getLength())
+                {
+                    if(strcmp(cpy_a.getBigint(), cpy_b.getBigint())>=0) return true;
+                    else return false;
+                }
+                else return true;
+            }
+        }
         if(cpy_a.getLength()<cpy_b.getLength()) return true;
         else if((cpy_a.getLength()==cpy_b.getLength())&&(strcmp(cpy_a.getBigint(), cpy_b.getBigint())<=0)) return true;
         else return false;
@@ -170,6 +307,21 @@ namespace BigInt_Yo
     bool operator <(const BigInt& a, const BigInt& b)
     {
         BigInt cpy_a(a), cpy_b(b);
+        if(cpy_a.getSign()==0||cpy_b.getSign()==0)
+        {
+            if(cpy_a.getSign()==1&&cpy_b.getSign()==0) return false;
+            else if(cpy_a.getSign()==0&&cpy_b.getSign()==1) return true;
+            else
+            {
+                if(cpy_a.getLength()<cpy_b.getLength()) return false;
+                else if(cpy_a.getLength()==cpy_b.getLength())
+                {
+                    if(strcmp(cpy_a.getBigint(), cpy_b.getBigint())>0) return true;
+                    else return false;
+                }
+                else return true;
+            }
+        }
         if(cpy_a.getLength()<cpy_b.getLength()) return true;
         else if((cpy_a.getLength()==cpy_b.getLength())&&(strcmp(cpy_a.getBigint(), cpy_b.getBigint())<0)) return true;
         else return false;
@@ -190,6 +342,7 @@ namespace BigInt_Yo
             }
             bigInteger[n] = '\0';
             length = rtSide.length;
+            sign = rtSide.sign;
             return *this;
         }
     }
@@ -197,7 +350,11 @@ namespace BigInt_Yo
     const BigInt operator %(const BigInt &a, const BigInt &b)
     {
         BigInt zero(0);
-        if(b==zero) exit(0);
+        if(b==zero)
+        {
+            cout << "is wrong!\n";
+            exit(0);
+        }
         BigInt q = a/b, r;
         r = a-(q*b);
         return BigInt(r);
@@ -206,13 +363,49 @@ namespace BigInt_Yo
     const BigInt operator /(const BigInt &a, const BigInt &b)
     {
         BigInt zero(0);
-        if(b==zero) exit(0);
+        if(b==zero)
+        {
+            cout << "is wrong!\n";
+            exit(0);
+        }
         BigInt result(0);
-        int j, bit;
+        int j, bit, ide = 0;
         BigInt cpy_a(a), cpy_b(b);
+        if(cpy_a.getSign()==0&&cpy_b.getSign()==1)
+        {
+            cpy_a.sign = 1;
+            ide = 1;
+        }
+        else if(cpy_a.getSign()==1&&cpy_b.getSign()==0)
+        {
+            cpy_b.sign = 1;
+            ide = 2;
+        }
+        else if(cpy_a.getSign()==0&&cpy_b.getSign()==0)
+        {
+            cpy_a.sign = 1;
+            cpy_b.sign = 1;
+            ide = 3;
+        }
         while(cpy_a>zero)
         {
-            if(cpy_a<b) return BigInt(result);
+            if(cpy_a<cpy_b)
+            {
+                if(ide==1||ide==2)
+                {
+                    char negResult[result.getLength()+2];
+                    negResult[0] = '-';
+                    for(j = 0; j<result.getLength(); j++)
+                    {
+                        negResult[j+1] = result.getBigint()[j];
+                    }
+                    negResult[j+1] = '\0';
+                    if(negResult[0]=='-'&&negResult[1]=='0') return BigInt(0);
+                    string finalResult = negResult;
+                    return BigInt(finalResult);
+                }
+                return BigInt(result);
+            }
             char tmp_a[cpy_b.getLength()+2];
             int i;
             memset(tmp_a, '0', cpy_b.getLength()+1);
@@ -244,13 +437,13 @@ namespace BigInt_Yo
                 BigInt tmpOfA(tmp_a);
                 for(i = 9;i>=0;i--)
                 {
-                    BigInt tmpi(i), ixb(tmpi*b);
+                    BigInt tmpi(i), ixb(tmpi*cpy_b);
                     if(ixb<=tmpOfA)
                     {
                         int tmpbit;
                         bit = cpy_a.getLength()-bit-1;
                         tmpbit = bit;
-                        BigInt pow10(1), q = tmpi*b, ten(10);
+                        BigInt pow10(1), q = tmpi*cpy_b, ten(10);
                         while(bit--)
                         {
                             pow10 = pow10*ten;
@@ -266,13 +459,13 @@ namespace BigInt_Yo
                 BigInt tmpOfA(tmp2_a);
                 for(i = 9;i>=0;i--)
                 {
-                    BigInt tmpi(i), ixb(tmpi*b);
+                    BigInt tmpi(i), ixb(tmpi*cpy_b);
                     if(ixb<=tmpOfA)
                     {
                         int tmpbit;
                         bit = cpy_a.getLength()-bit-1;
                         tmpbit = bit;
-                        BigInt pow10(1), q = tmpi*b, ten(10);
+                        BigInt pow10(1), q = tmpi*cpy_b, ten(10);
                         while(bit--)
                         {
                             pow10 = pow10*ten;
@@ -284,6 +477,19 @@ namespace BigInt_Yo
                 }
             }
         }
+        if(ide==1||ide==2)
+        {
+            char negResult[result.getLength()+2];
+            negResult[0] = '-';
+            for(j = 0; j<result.getLength(); j++)
+            {
+                negResult[j+1] = result.getBigint()[j];
+            }
+            negResult[j+1] = '\0';
+            if(negResult[0]=='-'&&negResult[1]=='0') return BigInt(0);
+            string finalResult = negResult;
+            return BigInt(finalResult);
+        }
         return BigInt(result);
     }
 
@@ -291,7 +497,23 @@ namespace BigInt_Yo
     {
         BigInt cpy_a(a), cpy_b(b);
         int *multi = new int[cpy_a.getLength()+cpy_b.getLength()];
-        int i, j, k, tempk, l;
+        int i, j, k, tempk, l, ide = 0;
+        if(cpy_a.getSign()==0&&cpy_b.getSign()==1)
+        {
+            cpy_a.sign = 1;
+            ide = 1;
+        }
+        else if(cpy_a.getSign()==1&&cpy_b.getSign()==0)
+        {
+            cpy_b.sign = 1;
+            ide = 2;
+        }
+        else if(cpy_a.getSign()==0&&cpy_b.getSign()==0)
+        {
+            cpy_a.sign = 1;
+            cpy_b.sign = 1;
+            ide = 3;
+        }
         for(j = 0;j<cpy_a.getLength()+cpy_b.getLength();j++)
         {
             multi[j] = 0;
@@ -344,6 +566,19 @@ namespace BigInt_Yo
         cpya = NULL;
         cpyb = NULL;
         multiNumber[m] = '\0';
+        if(ide==1||ide==2)
+        {
+            char negMulti[strlen(multiNumber)+2];
+            negMulti[0]='-';
+            for(j = 0; j<strlen(multiNumber); j++)
+            {
+                negMulti[j+1] = multiNumber[j];
+            }
+            negMulti[j+1] = '\0';
+            if(multiNumber[0]=='-'&&multiNumber[1]=='0') return BigInt(0);
+            string returnNumber = negMulti;
+            return BigInt(returnNumber);
+        }
         string returnNumber = multiNumber;
         return BigInt(returnNumber);
     }
@@ -351,8 +586,23 @@ namespace BigInt_Yo
     const BigInt operator +(const BigInt &a, const BigInt &b)
     {
         BigInt cpy_a(a), cpy_b(b);
-        if(cpy_a==BigInt(0)&&cpy_b==BigInt(0)) return BigInt(0);
-        int m, n, maxLength = (cpy_a.getLength()>=cpy_b.getLength()?cpy_a.getLength():cpy_b.getLength());
+        int m, n, j, maxLength = (cpy_a.getLength()>=cpy_b.getLength()?cpy_a.getLength():cpy_b.getLength());
+        if(cpy_a.getSign()==0&&cpy_b.getSign()==1)
+        {
+            cpy_a.sign = 1;
+            return BigInt(cpy_b-cpy_a);
+        }
+        else if(cpy_a.getSign()==1&&cpy_b.getSign()==0)
+        {
+            cpy_b.sign = 1;
+            return BigInt(cpy_a-cpy_b);
+        }
+        else if(cpy_a.getSign()==0&&cpy_b.getSign()==0)
+        {
+            cpy_a.sign = 1;
+            cpy_b.sign = 1;
+            return BigInt(-(cpy_a+cpy_b));
+        }
         char *cpyNumber, *addNumber;
         addNumber = new char[maxLength+2];
         memset(addNumber, '0', maxLength+1);
@@ -428,7 +678,23 @@ namespace BigInt_Yo
     const BigInt operator -(const BigInt &a, const BigInt &b)
     {
         BigInt cpy_a(a), cpy_b(b);
-        int ide, m, n, maxLength = (cpy_a.getLength()>=cpy_b.getLength()?cpy_a.getLength():cpy_b.getLength());
+        int ide, m, n, j, maxLength = (cpy_a.getLength()>=cpy_b.getLength()?cpy_a.getLength():cpy_b.getLength());
+        if(cpy_a.getSign()==0&&cpy_b.getSign()==1)
+        {
+            cpy_a.sign = 1;
+            return BigInt(-(cpy_a+cpy_b));
+        }
+        else if(cpy_a.getSign()==1&&cpy_b.getSign()==0)
+        {
+            cpy_b.sign = 1;
+            return BigInt(cpy_a+cpy_b);
+        }
+        else if(cpy_a.getSign()==0&&cpy_b.getSign()==0)
+        {
+            cpy_a.sign = 1;
+            cpy_b.sign = 1;
+            return BigInt(cpy_b-cpy_a);
+        }
         char *cpyNumber, *minusNumber;
         minusNumber = new char[maxLength+1];
         memset(minusNumber, '0', maxLength);
@@ -481,11 +747,11 @@ namespace BigInt_Yo
             {
                 minuserNumber[m++] = minusNumber[n];
             }
+            minuserNumber[m] = '\0';
             delete [] minusNumber;
             delete [] cpyNumber;
             minusNumber = NULL;
             cpyNumber = NULL;
-            minuserNumber[m] = '\0';
             string returnNumber = minuserNumber;
             return BigInt(returnNumber);
         }
@@ -524,19 +790,33 @@ namespace BigInt_Yo
             {
                 minuserNumber[m++] = minusNumber[n];
             }
+            minuserNumber[m] = '\0';
             delete [] minusNumber;
             delete [] cpyNumber;
             minusNumber = NULL;
             cpyNumber = NULL;
-            minuserNumber[m] = '\0';
             string returnNumber = minuserNumber;
             return BigInt(returnNumber);
         }
     }
 
-    const BigInt BigInt::factorial(string factor)
+    const BigInt operator -(const BigInt &a)
     {
-        int l = strlen(factor.c_str());//cout << l << endl;
+        BigInt cpy_a(a);
+        if(cpy_a.sign==0)
+        {
+            cpy_a.sign = 1;
+        }
+        else
+        {
+            cpy_a.sign = 0;
+        }
+        return BigInt(cpy_a);
+    }
+
+    const BigInt factorial(string factor)
+    {
+        int l = strlen(factor.c_str());
         char cpytmp[l], cpy[l-1];
         strcpy(cpytmp, factor.c_str());
         int i = 0, j = 1;
@@ -545,13 +825,11 @@ namespace BigInt_Yo
             cpy[i] = cpytmp[i];
         }
         i = atoi(cpy);
-        BigInt tmp(1), result(1);//cout<< i << " " << j <<endl;
+        BigInt tmp(1), result(1);
         for(;j<=i;j++)
         {
-            BigInt *tmp = new BigInt(j);
-            result = result * (*tmp);
-            delete tmp;
-            tmp = NULL;
+            BigInt tmp(j);
+            result = result * tmp;
         }
         return BigInt(result);
     }
